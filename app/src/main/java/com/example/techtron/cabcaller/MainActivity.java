017d0512b1ac24c90a3032a08a570f9d55452936
Phillip <phillipables1@gmail.com>
2018-03-24 22:23:46 -0400

    mainactivity java-> declare variables that are used on the subactivity, driver picture name plate number and also declare the button, instantiate and set to there ids

--- app/src/main/java/com/example/techtron/cabcaller/MainActivity.java
+++ app/src/main/java/com/example/techtron/cabcaller/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.techtron.cabcaller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewDriver, textViewPlateNum;
+
+    ImageView imageViewDriver;
+
+    Button buttonInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewDriver = (TextView) findViewById(R.id.textViewDriver);
+        textViewPlateNum = (TextView) findViewById(R.id.textViewPlateNum);
+
+        imageViewDriver = (ImageView) findViewById(R.id.imageViewDriver);
+
+        buttonInfo = (Button) findViewById(R.id.buttonInfo);
     }
 }
--- app/src/main/java/com/example/techtron/cabcaller/SubActivity.java
+++ app/src/main/java/com/example/techtron/cabcaller/SubActivity.java
@@ -2,9 +2,9 @@ package com.example.techtron.cabcaller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
