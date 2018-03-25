ea0992a060182114fcd9e95dbcc47dead93ca11e
Phillip <phillipables1@gmail.com>
2018-03-24 23:18:52 -0400

    ActivityOptions of makeSceneTransitionAnimation for this MainActivity, create Pair for id and name of transition

--- app/src/main/java/com/example/techtron/cabcaller/MainActivity.java
+++ app/src/main/java/com/example/techtron/cabcaller/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.techtron.cabcaller;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,7 +14,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewDriver, textViewPlateNum;
+    TextView textViewFirstName, textViewPlateNum;
 
     ImageView imageViewDriver;
 
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewDriver = (TextView) findViewById(R.id.textViewDriver);
+        textViewFirstName = (TextView) findViewById(R.id.textViewFirstName);
         textViewPlateNum = (TextView) findViewById(R.id.textViewPlateNum);
 
         imageViewDriver = (ImageView) findViewById(R.id.imageViewDriver);
@@ -29,13 +33,17 @@ public class MainActivity extends AppCompatActivity {
         buttonInfo = (Button) findViewById(R.id.buttonInfo);
 
         buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SubActivity.class);
 
+                Pair<View,String> pairDriver = Pair.create(findViewById(R.id.imageViewDriver), "driver");
+                Pair<View,String> pairName = Pair.create(findViewById(R.id.textViewFirstName), "firstName");
+                Pair<View,String> pairNum = Pair.create(findViewById(R.id.textVIewPlateNum), "plateNum")
+
+                ActivityOptions acitivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
                 startActivity(intent);
-
-
             }
         });
 
